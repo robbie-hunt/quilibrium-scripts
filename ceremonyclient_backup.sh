8935c9067aa5469f2d86f79098b3f4853aa83882
Robbie Hunt <robbiehunt@Robbies-MBP.station>
2024-12-23 19:57:46 +0000

    updates

--- ceremonyclient_backup.sh
+++ ceremonyclient_backup.sh
@@ -8,17 +8,14 @@ USAGE_func() {
     echo ""
     echo "Backup tool for the Quilibrium node."
     echo ""
-    echo "USAGE: bash ceremonyclient_backup.sh [-h] [-x] [-c]"
+    echo "USAGE: bash ceremonyclient_backup.sh [-h] [-x]"
     echo ""
     echo "       -h    Display this help dialogue."
     echo "       -x    For debugging the script; sets the x shell builtin, 'set -x'."
-    echo "       -c    (Optional) This node is part of a cluster."
-    echo "             (By default this is set to null, meaning this node is run as a standalone node.)"
-    echo "       -d    (Optional) Directory to update binaries in."
-    echo "             By default, the directory to install to is determined by the 'ceremonyclient_node_dir' key in .localenv."
     echo ""
     exit 0
 }
+
 dateAndTime=$(date +%y%m%d-%H:%M:%S)
 #node=`find /root/ceremonyclient/node -maxdepth 1 -type f -executable -name 'node-*-linux-amd64' -ls | head -1 | awk '{print $NF}'`
 #peerID=`$node -peer-id | grep -oP '^Peer ID: \K\w+'`
