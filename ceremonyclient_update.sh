8d568b5ad49671f8a19804d717f6a03de7dcf7e8
Robbie Hunt <robbiehunt@Robbies-MBP.station>
2025-01-10 16:57:41 +0000

    updates

--- ceremonyclient_update.sh
+++ ceremonyclient_update.sh
@@ -137,9 +137,9 @@ else
 fi
 
 # The OS of the machine running this script
-RELEASE_OS=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh.sh -os)
+RELEASE_OS=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh -os)
 # The release line ('os-arch') of the machine running this script
-RELEASE_LINE=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh.sh -release-line)
+RELEASE_LINE=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh -release-line)
 
 # For the ceremonyclient node directory
 # If a directory was supplied via the -d option, use it
@@ -155,10 +155,10 @@ CEREMONYCLIENT_LOGFILE="$HOME/ceremonyclient.log"
 
 # Get the latest version of the main node and qclient binaries,
 # both installed and available in release
-LATEST_NODE_INSTALLED=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh.sh -latest-version 'node-install-files-quiet')
-LATEST_NODE_RELEASE=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh.sh -latest-version 'node-release-files-quiet')
-LATEST_QCLIENT_INSTALLED=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh.sh -latest-version 'qclient-install-files-quiet')
-LATEST_QCLIENT_RELEASE=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh.sh -latest-version 'node-release-files-quiet')
+LATEST_NODE_INSTALLED=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh -latest-version 'node-install-files-quiet')
+LATEST_NODE_RELEASE=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh -latest-version 'node-release-files-quiet')
+LATEST_QCLIENT_INSTALLED=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh -latest-version 'qclient-install-files-quiet')
+LATEST_QCLIENT_RELEASE=$(. $SCRIPT_DIR/tools/ceremonyclient_env.sh -latest-version 'node-release-files-quiet')
 
 COMPARE_VERSIONS_func "$LATEST_NODE_INSTALLED" "$LATEST_NODE_RELEASE"
 COMPARE_VERSIONS_func "$LATEST_QCLIENT_INSTALLED" "$LATEST_QCLIENT_RELEASE"
