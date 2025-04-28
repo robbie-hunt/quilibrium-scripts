02669a23d88ad9e2de881971267b826206f0e747
Robbie Hunt <robbiehunt@Robbies-MacBook-Pro.local>
2025-04-28 15:39:37 +0100

    updates

--- ceremonyclient_backup.sh
+++ ceremonyclient_backup.sh
@@ -27,15 +27,55 @@ SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 SCRIPT_PARENT_DIR=$(echo "$SCRIPT_DIR" | awk -F'/' 'BEGIN{OFS=FS} {$NF=""; print}' | sed 's/\/*$//')
 
 TIMESTAMP=$(date +%y%m%d-%H:%M:%S)
+
 HOSTNAME=$(hostname)
 PEER_ID=$(bash $SCRIPT_DIR/tools/ceremonyclient_env.sh -key "peer_id")
+
 CEREMONYCLIENT_ROOT_DIR=$(bash $SCRIPT_DIR/tools/ceremonyclient_env.sh -key "ceremonyclient_root_dir")
 CEREMONYCLIENT_CONFIG_DIR=$(bash $SCRIPT_DIR/tools/ceremonyclient_env.sh -key "ceremonyclient_config_dir")
 BACKUP_PARENT_DIR=$(dirname "$CEREMONYCLIENT_ROOT_DIR")
 BACKUP_DIR=$(echo "$BACKUP_PARENT_DIR/ceremonyclient_backup")
-RCLONE_PARENT_DIR="rhquil2:Quilibrium"
+RSYNC_LOGFILE="$BACKUP_DIR/rsync_$TIMESTAMP.log"
+RCLONE_PARENT_DIR="rhquil2:Quilibrium/$PEER_ID/$HOSTNAME"
 
+# Check if backup dir exists
+    # If yes, continue
+    # If no, make it
+# Copy config files to folder via rsync, direct rsync log into file which has filename of rsync_timestamp.log
+# Zip folder
+# Rclone folder to rhquil2, direct log to rclone_timestamp.log
 
+# Create backup dir if it doesn't exist
+if [[ -d "$BACKUP_DIR" ]]; then
+    :
+else
+    mkdir "$BACKUP_DIR"
+fi
+
+# Reset rsync logfile
+echo $TIMESTAMP > "$RSYNC_LOGFILE"
+printf "\n" >> "$RSYNC_LOGFILE"
+
+# rsync - copy over config files, excluding store folder
+if rsync -avhpR -n --exclude "store" "$CEREMONYCLIENT_CONFIG_DIR" "$BACKUP_DIR" &>/dev/null; then
+    rsync -avhpR --exclude "store" "$CEREMONYCLIENT_CONFIG_DIR" "$BACKUP_DIR" >> "$RSYNC_LOGFILE"
+else
+    echo "ceremonyclient_backup.sh error [$(date)]: rsync command 'rsync -avhpR -n --exclude store $CEREMONYCLIENT_CONFIG_DIR $BACKUP_DIR' failed. Exiting..."
+    exit
+fi
+
+# Zip backup dir
+zip -X $BACKUP_DIR $BACKUP_DIR
+
+# rclone - copy zipped backup dir to Dropbox
+if rclone -n copy "$BACKUP_DIR".zip "$RCLONE_PARENT_DIR" &>/dev/null; then
+    rclone -n copy "$BACKUP_DIR".zip "$RCLONE_PARENT_DIR"
+else
+    echo "ceremonyclient_backup.sh error [$(date)]: rclone command 'rclone -n copy "$BACKUP_DIR".zip "$RCLONE_PARENT_DIR"' failed. Exiting..."
+    exit
+fi
+
+exit
 
 
 
@@ -48,4 +88,4 @@ tar -czf /root/ceremonyclient_backup.tar.gz /root/ceremonyclient_backup
 
 rclone copy /root/ceremonyclient_backup.tar.gz rhquil2:$USER/node1
 
-exit 0
+exit
